f499a160fcdb4e855cbd48cd45440ea82403008e
Loma <lomakim@gmail.com>
2026-02-12 19:24:51 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 const int SIZE = 5;
 
 //Function Prototypes
-void enterArrayData(double* []);
-// void outputArrayData(double* []);
+void enterArrayData(double []);
+void outputArrayData(double []);
 // double sumArray(double* []);
 
 int main(){
@@ -14,13 +14,32 @@ int main(){
     double *ptr = nullptr;
     ptr = new double[SIZE];
 
+    // Input array data
+    enterArrayData(ptr);
+
+    // Ouptut array data
+
     delete [] ptr;
 
     return 0;
 }
 
 void enterArrayData(double* arr) {
+//    double *tempArr = nullptr;
+//    tempArr = new double[SIZE];
+    cout << "Data entry for the array:\n";
+    for (int i = 0; i < SIZE; i++){
+        cout << "\t> Element #" << i << ": ";
+        cin >> *(arr + i);
+    }
+    cout << "Data entry complete.\n";
 
 }
-// void outputArrayData(double* []) {}
+void outputArrayData(double* arr) {
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < SIZE; i++)
+        cout << *(arr + i) << " ";
+    
+    cout << endl;
+}
 // double sumArray(double* []); {}
