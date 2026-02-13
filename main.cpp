19c084d46ccfec4651e8670231d11b6149ce1455
Loma <lomakim@gmail.com>
2026-02-12 19:13:07 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -2,7 +2,25 @@
 #include <iostream>
 using namespace std;
 
+const int SIZE = 5;
+
+//Function Prototypes
+void enterArrayData(double* []);
+// void outputArrayData(double* []);
+// double sumArray(double* []);
+
 int main(){
+    //Declarations
+    double *ptr = nullptr;
+    ptr = new double[SIZE];
+
+    delete [] ptr;
 
     return 0;
 }
+
+void enterArrayData(double* arr) {
+
+}
+// void outputArrayData(double* []) {}
+// double sumArray(double* []); {}
